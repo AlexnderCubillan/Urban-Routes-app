841fdee70828eb53bab57f2abcfbf62cee6c71d4
alexa <pagoinstantaneovenezuela@gmail.com>
2024-05-15 18:25:47 -0400

    Urban Router App

---
+++ data.py
@@ -0,0 +1,6 @@
+urban_routes_url = ('https://cnt-bb855b37-8de4-47bf-9c96-f696d8cba4e8.containerhub.tripleten-services.com?lng=es' )
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
